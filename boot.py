f2b8e5a8c3a283226139142539ad0ddb9333486c
Greg Cope <gregcope@gmail.com>
2018-04-15 18:32:30 +0100

    Stripped boot.py

---
+++ boot.py
@@ -0,0 +1,18 @@
+from machine import UART
+import machine
+from pytrack import Pytrack
+import utime
+import pycom
+
+start = utime.ticks_us()
+uart = UART(0, baudrate=115200)
+os.dupterm(uart)
+
+# Initialise some other objects
+py = Pytrack()
+
+machine.main('main.py')
+
+end = utime.ticks_us()
+took = end - start
+print("boot.py ... done in: {} uSec".format(took))
